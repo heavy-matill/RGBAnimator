c0b95f182a97b3faecd1ff37def6c21070dbe314
heavy-matill <mathe23491@gmx.de>
2018-06-12 18:36:49 +0200

    added drawing of color

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "RGBAnimator.cpp"
 
 using namespace std;
+void paint(int r, int g, int b);
+void paint_color( int c, int i);
 
 int main()
 {
@@ -16,7 +18,31 @@ int main()
     animor.RGBTaskList.push_back(flt1);  
     RGBTask* task = *animor.RGBTaskList.begin();
     task->GetAnimation()->Update(10);
+    paint(200, 50, 100);
     getchar();
     return 0;
 
+}
+void paint(int r, int g, int b)
+{
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    for(int i = 256-16;i>0; i-=16)
+    {
+        paint_color(r,i);
+        paint_color(g,i);
+        paint_color(b,i);
+        cout<<"\n";
+    }
+    cout<<"---   ---   ---   \n R     G     B    ";
+}
+void paint_color( int c, int i)
+{
+    if (c>i)
+    {
+        cout<<"###   ";
+    }
+    else
+    {
+        cout<<"      ";
+    }
 }

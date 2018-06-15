79872e49192d586176985e1db0b5c80a6a84f5e1
heavy-matill <mathe23491@gmx.de>
2018-06-15 19:59:35 +0200

    renamed paint functions

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include "RGBAnimator.cpp"
 
 using namespace std;
-void paint(int r, int g, int b);
-void paint_color( int c, int i);
+void paint_rgb(int r, int g, int b);
+void paint_c( int c, int i);
 
 int main()
 {
@@ -18,24 +18,24 @@ int main()
     animor.RGBTaskList.push_back(flt1);  
     RGBTask* task = *animor.RGBTaskList.begin();
     //task->GetAnimation()->Update(10);
-    paint(200, 50, 100);
+    paint_rgb(200, 50, 100);
     getchar();
     return 0;
 
 }
-void paint(int r, int g, int b)
+void paint_rgb(int r, int g, int b)
 {
     cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     for(int i = 256-16;i>0; i-=16)
     {
-        paint_color(r,i);
-        paint_color(g,i);
-        paint_color(b,i);
+        paint_c(r,i);
+        paint_c(g,i);
+        paint_c(b,i);
         cout<<"\n";
     }
     cout<<"---   ---   ---   \n R     G     B    ";
 }
-void paint_color( int c, int i)
+void paint_c( int c, int i)
 {
     if (c>i)
     {
